f41151fbc4901329575fda1da69a22500c65463c
willihane <williane.pereira@outlook.com>
2022-05-13 17:03:40 -0300

    Atualizando blog com testes junit

--- src/main/java/com/blogpessoal/blogpessoal/model/Usuario.java
+++ src/main/java/com/blogpessoal/blogpessoal/model/Usuario.java
@@ -19,17 +19,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@NotNull(message = "O atributo Nome é Obrigatório!")
 	private String nome;
-
-	@Size(max = 5000, 
-	message = "O link da foto não pode ser maior do que 5000 caracteres")
-	private String foto;
 
 	@NotNull(message = "O atributo Usuário é Obrigatório!")
 	@Email(message = "O atributo Usuário deve ser um email válido!")
@@ -39,11 +35,34 @@ public class Usuario {
 	@Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
 	private String senha;
 
+	private String foto;
+	
+	private String tipo;
+
+	
 	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
+	
+	public Usuario(Long id, String nome, String usuario, String senha, String foto, String tipo) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+		this.tipo = tipo;
+	}
+	
+	public Usuario() { }
 
-	/* Insira os Getters and Setters */
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -60,14 +79,6 @@ public class Usuario {
 		this.nome = nome;
 	}
 
-	public String getFoto() {
-		return foto;
-	}
-
-	public void setFoto(String foto) {
-		this.foto = foto;
-	}
-
 	public String getUsuario() {
 		return usuario;
 	}
@@ -82,6 +93,14 @@ public class Usuario {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
 	}
 
 	public List<Postagem> getPostagem() {
--- src/main/java/com/blogpessoal/blogpessoal/model/UsuarioLogin.java
+++ src/main/java/com/blogpessoal/blogpessoal/model/UsuarioLogin.java
@@ -1,13 +1,41 @@
 package com.blogpessoal.blogpessoal.model;
 
 public class UsuarioLogin {
-	
+
 	private Long id;
+
 	private String nome;
+
 	private String usuario;
-	private String foto;
+
 	private String senha;
+
+	private String foto;
+	
+	private String tipo;
+
 	private String token;
+	
+	
+
+	public UsuarioLogin(Long id, String nome, String usuario, String senha, String foto, String tipo, String token) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+		this.tipo = tipo;
+		this.token = token;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 
 	public Long getId() {
 		return id;
@@ -21,22 +49,12 @@ public class UsuarioLogin {
 		return nome;
 	}
 
-
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
 	public String getUsuario() {
 		return usuario;
-	}
-	
-	public String getFoto() {
-		return foto;
-	}
-
-	public void setFoto(String foto) {
-		this.foto = foto;
 	}
 
 	public String getToken() {
@@ -58,4 +76,13 @@ public class UsuarioLogin {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}	
+	
 }
